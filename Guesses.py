96a377aa1d3574d7e5f1480513c2b73d5bde037c
Jeremy Stiff <jstiff@ggc.edu>
2022-02-19 17:41:58 -0500

    Refactoring codebase into discrete elements

--- Guesses.py
+++ Guesses.py
@@ -1,13 +1,4 @@
-import numpy
-
-# A list of English letters as well as the arrays to
-# store words that are in the word or not
-vowels:str = ['a', 'e', 'i', 'o', 'u']
-consonants:str = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k',
-              'l', 'm', 'n', 'p', 'q', 'r', 's', 't',
-              'v', 'w', 'x', 'y', 'z']
-included = []
-excluded = []
+import WordSelection
 
 def getInput():
     print("Enter a five letter string")
@@ -15,26 +6,6 @@ def getInput():
     if (len(userin) != 5):
         print("The string you entered was not five letters")
         getInput()
-    return userin
-
-# Function determines the value of a guess where 10 points are given for a letter
-# existing in a word and an additional 10 points for the letter in the correct place
-def fitnessCheck(userword:str, aiword:str):
-    fitness:int = 0
-    indexpos = 0
-    for letter in aiword:
-        if (letter not in userword and letter not in excluded):
-            excluded.append(letter)
-        if letter in userword:
-            if letter in included and letter == userword[indexpos]:
-                fitness += 20
-            if letter not in included:
-                fitness += 10
-                included.append(letter)
-                if letter == userword[indexpos]:
-                    fitness += 10
-        indexpos += 1
-    return fitness
-            
+    return userin            
     
-print(fitnessCheck('ttttt', 'ttttt'))
+print(WordSelection.fitnessCheck('ttttt', 'ttttt'))
