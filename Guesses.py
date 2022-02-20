6be987b18c47d79c01a66d56b8221ac20254dab3
Jeremy Stiff <jstiff@ggc.edu>
2022-02-20 14:51:57 -0500

    initial population, mating, and mutation complete

--- Guesses.py
+++ Guesses.py
@@ -1,11 +1,18 @@
+import random
 import WordSelection
 
 def getInput():
     print("Enter a five letter string")
-    userin:str = input().lower
+    userin:str = input().lower()
     if (len(userin) != 5):
         print("The string you entered was not five letters")
-        getInput()
+        userin = getInput()
     return userin            
-    
-print(WordSelection.fitnessCheck('ttttt', 'ttttt'))
+
+WordSelection.target = 'trunk'
+pop = WordSelection.generateGroups()
+children = []
+for i in pop:
+    children.append(WordSelection.competitionSelection(i))
+secondgen = WordSelection.competitionSelection(children)
+print("Best second gen child is: ", secondgen, " with fitness of: ", WordSelection.fitnessCheck(secondgen))
--- WordSelection.py
+++ WordSelection.py
@@ -1,29 +1,30 @@
 import random
 # A list of English letters as well as the arrays to
 # store words that are in the word or not
-vowels:str = ['a', 'e', 'i', 'o', 'u']
-consonants:str = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k',
+vowels = ['a', 'e', 'i', 'o', 'u']
+consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k',
               'l', 'm', 'n', 'p', 'q', 'r', 's', 't',
               'v', 'w', 'x', 'y', 'z']
+target = ""
 included = []
 excluded = []
 fitness = 0
 
 # Function determines the value of a guess where 10 points are given for a letter
 # existing in a word and an additional 10 points for the letter in the correct place
-def fitnessCheck(userword:str, aiword:str):
+def fitnessCheck(aiword):
     fitness:int = 0
     indexpos = 0
     for letter in aiword:
-        if (letter not in userword and letter not in excluded):
+        if (letter not in target and letter not in excluded):
             excluded.append(letter)
-        if letter in userword:
-            if letter in included and letter == userword[indexpos]:
+        if letter in target:
+            if letter in included and letter == target[indexpos]:
                 fitness += 20
             if letter not in included:
                 fitness += 10
                 included.append(letter)
-                if letter == userword[indexpos]:
+                if letter == target[indexpos]:
                     fitness += 10
         indexpos += 1
     return fitness
@@ -34,3 +35,53 @@ def randomLetter():
         return vowels[random.randrange(0,4)]
     else:
         return consonants[random.randrange(0, 20)]
+    
+# Initial guess is random
+def initialGuess():
+    out = ""
+    for i in range(5):
+        out += randomLetter()
+    return out
+
+# 40 groups of 40 will compete to be the guess
+def generateGroups():
+    battleRoyale = []
+    for i in range(40):
+        group = []
+        for x in range(40):
+            group.append(initialGuess())
+        battleRoyale.append(group)
+    return battleRoyale
+
+# Takes in an array of words and selects the most fit parents and produces a child
+def competitionSelection(inputPop):
+    mother = "abcde"
+    father = "fghij"
+    for x in inputPop:
+        if fitnessCheck(x) > fitnessCheck(mother):
+            father = mother
+            mother = x
+        if fitnessCheck(x) == fitnessCheck(mother):
+            if random.random() >= 0.50:
+                father = mother
+                mother = x
+    return matingPairs(mother, father)
+
+# Mates a pair and gives the child the opportunity to mutate
+# Mother gives first 2 letters father gives 3rd and 4th
+# 5th letter is coinflipped between the two
+def matingPairs(mother:str, father):
+    MUTATION_RATE = 0.015
+    child = mother[:2] + father[2:-1]
+    if random.random() >= 0.50:
+        child += mother[-1:]
+    else:
+        child += father[-1:]
+    pos = 0
+    for x in child:
+        if random.random() <= MUTATION_RATE:
+            child = child[:pos] + randomLetter() + child[pos+1:]
+        pos += 1
+    
+    return child
+    
