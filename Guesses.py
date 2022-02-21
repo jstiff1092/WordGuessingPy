aa0d0e07bca31c952a0235299b00b3c68b276a4f
Jeremy Stiff <jstiff@ggc.edu>
2022-02-20 22:05:09 -0500

    This version sorta sucks

--- Guesses.py
+++ Guesses.py
@@ -1,28 +1,93 @@
-from operator import contains
+from ctypes.wintypes import tagRECT
 import WordSelection
+# My origional attempts to solve the genetic algorithm all ended up with the guesses not making any progress
+# This attempt at a successful version was implimented based on https://www.geeksforgeeks.org/genetic-algorithms/
 
+# Gets the user's input for the purpose of setting the target word
 def getInput():
     print("Enter a five letter string")
     userin:str = input().lower()
     if (len(userin) != 5):
         print("The string you entered was not five letters")
         userin = getInput()
-    return userin            
+    return userin      
 
-WordSelection.target = 'pizza'
-pop = WordSelection.generateGroups()
-children = []
-attempts = 0
+# Runs a generation like this: !ORIGIONAL ATTEMPT!
+# 1. Pop of 40*40 is broken into 40 pairs of parents and mated to create 40 children
+# 2. Group of 40 children is culled to remove those with 0 fitness since they have no chance to progress the guess to target
+# 3. Group of culled children are mated to produce a healthy group of 40
+# 4. The healthy group is added to a new pop of 9 random groups
+def cycleGeneration(pop):
+    children = WordSelection.runGeneration(pop)
+    children = WordSelection.cullWeak(children)
+    children = WordSelection.repopulatePop(children)
+    return WordSelection.generateGroupsFromPop(children)
 
-while 'pizza' not in children:
-    pop = WordSelection.generateGroups()
-    for i in pop:
-        parents = WordSelection.competitionSelection(i)
-        children.append(WordSelection.matingPairs(parents[0], parents[1]))
-        children = WordSelection.cullWeak(children)
-        children = WordSelection.repopulatePop(children)
-    print("Guess: ", WordSelection.competitionSelection(children)[0])
-    print("Attempts: ", attempts)
-    attempts += 1
+# Get target and generate initial group
+def initilizePop():
+    WordSelection.target = getInput()
+    return WordSelection.generateGroups()
+
+#pop = initilizePop()
+#children = WordSelection.runGeneration(pop)
+#guess = ""
+#attempt = 0
+#while guess != WordSelection.target:
+#    children = WordSelection.cullWeak(children)
+#    children = WordSelection.repopulatePop(children)
+#    print("Guess: ", WordSelection.competitionSelection(children))
+#    print("attempt: ", attempt)
+#    attempt += 1
+
+# My origional attempts to solve the genetic algorithm all ended up with the guesses not making any progress
+# This attempt at a successful version was implimented based on https://www.geeksforgeeks.org/genetic-algorithms/
+population = 100
+
+def main():
+    global population
     
-# TODO have the repopulated pop of 40 compete with 39 other groups of new pops
+    attempts = 1
+    
+    found = False
+    pop = []
+    WordSelection.target = getInput()
+    
+    # Generate initial pop
+    for _ in range(population):
+        individual = WordSelection.initialGuess()
+        pop.append(individual)
+        
+    while not found:
+        
+        # Cull unfit pop
+        pop = WordSelection.cullWeakImproved(pop)
+
+        # Repopulate Pop
+        pop = WordSelection.repopulatePopImproved(pop)
+        
+        # Get fresh genome from new pop
+        newPop = []
+        for _ in range(population):
+            newPop.append(WordSelection.initialGuess())
+        pos = 0
+        temp = []
+        for x in pop:
+            temp.append("".join(WordSelection.matingPairImproved(x, newPop[pos])))
+            pos += 1
+        pop = temp
+    
+        print("Generation: ", attempts)
+        best = WordSelection.competitionSelectionImproved(pop)[0]
+        print("Best: ", best)
+        if best == WordSelection.target:
+            found = True
+            break
+        if attempts == 5:
+            print("Do you want to continue? y/n")
+            if input() == 'n':
+                break
+        attempts += 1
+    
+        
+if __name__ == '__main__':
+    main()
--- WordSelection.py
+++ WordSelection.py
@@ -36,7 +36,7 @@ def randomLetter():
     else:
         return consonants[random.randrange(0, 20)]
     
-# Initial guess is random
+# Initial guess is random, THIS IS THE RANDOM WORD GENERATION
 def initialGuess():
     out = ""
     for i in range(5):
@@ -55,12 +55,19 @@ def generateGroups():
 
 def generateGroupsFromPop(inputPop):
     battleRoyale = [inputPop]
-    for i in range(39):
+    for i in range(9):
         group = []
-        for x in range(39):
+        for x in range(40):
             group.append(initialGuess())
         battleRoyale.append(group)
     return battleRoyale
+
+# Takes the initial pop of 40*40 and breaks it into 40 pairs of parents, then mates the parents to create 40 children
+def runGeneration(inputPopArray):
+    children = []
+    for x in inputPopArray:
+        children.append(competitionSelection((x)))
+    return children
 
 # Takes in an array of words and selects the most fit parents and produces a child
 def competitionSelection(inputPop):
@@ -74,21 +81,45 @@ def competitionSelection(inputPop):
             if random.random() >= 0.50:
                 father = mother
                 mother = x
-    return [mother, father]
+    return matingPairs(mother, father)
+
+# Improved to use the new improved functions
+def competitionSelectionImproved(inputPop):
+    mother = "abcde"
+    father = "fghij"
+    for x in inputPop:
+        if fitnessCheck(x) > fitnessCheck(mother):
+            father = mother
+            mother = x
+        if fitnessCheck(x) == fitnessCheck(mother):
+            if random.random() >= 0.50:
+                father = mother
+                mother = x
+    return (mother, father)
 
 # Function to remove guesses with fitness <= 20
 def cullWeak(inputPop):
     newPop = []
     for x in inputPop:
-        if fitnessCheck(x) > 20:
+        if fitnessCheck(x) <= 20:
+            newPop.append(x)
+    return newPop
+
+# Version of cull that uses improved method
+def cullWeakImproved(inputPop):
+    newPop = []
+    for x in inputPop:
+        if fitnessCheckImproved(x) < 5:
             newPop.append(x)
     return newPop
 
 # Mates a pair and gives the child the opportunity to mutate
 # Mother gives first 2 letters father gives 3rd and 4th
 # 5th letter is coinflipped between the two
-def matingPairs(mother, father):
+def matingPairs(mother, father) -> str:
     MUTATION_RATE = 0.015
+    #mother = parentsPair[0]
+    #father = parentsPair[1]
     child = mother[:2] + father[2:-1]
     if random.random() >= 0.50:
         child += mother[-1:]
@@ -97,14 +128,58 @@ def matingPairs(mother, father):
     pos = 0
     for x in child:
         if random.random() <= MUTATION_RATE:
-            child = child[:pos] + randomLetter() + child[pos+1:]
+            temp = child[:pos] + randomLetter() + child[pos+1:]
+            child = temp
         pos += 1
-    
     return child
 
+
+# Improved version of above method referenced from https://www.geeksforgeeks.org/genetic-algorithms/
+def matingPairImproved(mother, father):
+    child = list()
+    for i in range(0, 5):
+        ran = random.random()
+        if ran < 0.45:
+           child.append(mother[i])
+        elif ran < 0.90:
+           child.append(father[i])
+        else:
+            child.append(randomLetter())
+    return child
+
+# Improved method referenced from https://www.geeksforgeeks.org/genetic-algorithms/
+def fitnessCheckImproved(aiword):
+    global target
+    fitness = 0
+    for gs, gt in zip(aiword, target):
+        if gs != gt: fitness += 1
+    return fitness
+
 # Repopulates a culled population to 40 by mating the remaining pairs randomly
+# Regenerates an empty list or single parent list to have 2 parents
 def repopulatePop(inputPop):
+    if not inputPop:
+        inputPop.append(initialGuess())
+        inputPop.append(initialGuess())
+    if len(inputPop) == 1:
+        inputPop.append(initialGuess())
     tempPop = inputPop
     while(len(tempPop) < 40):
+        #parents = (tempPop[random.randrange(0, len(tempPop))], tempPop[random.randrange(0, len(tempPop))])
         tempPop.append(matingPairs(tempPop[random.randrange(0, len(tempPop))], tempPop[random.randrange(0, len(tempPop))]))
+    return tempPop
+
+# Improved based on new matingpair method and parent selection based on https://www.geeksforgeeks.org/genetic-algorithms/
+def repopulatePopImproved(inputPop):
+    if not inputPop:
+        inputPop.append(initialGuess())
+        inputPop.append(initialGuess())
+    if len(inputPop) == 1:
+        inputPop.append(initialGuess())
+    tempPop = inputPop
+    while(len(tempPop) < 100):
+        parent1 = tempPop[random.randrange(0, len(tempPop))]
+        parent2 = tempPop[random.randrange(0, len(tempPop))]
+        child = "".join(matingPairImproved(parent1, parent2))
+        tempPop.append(child)
     return tempPop
